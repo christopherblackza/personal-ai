15cbcfc92737b2f1d3c3766f6464f221d9ca2216
Christopher <chris@propartners.co.za>
2024-01-10 00:17:33 +0700

    Clear previous Chat Messages

--- app.py
+++ app.py
@@ -55,12 +55,15 @@ def handle_userinput(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
 
+     # Clear the previous chat display
+    chat_display = st.empty()
+
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
-            st.write(user_template.replace(
+            chat_display.write(user_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
         else:
-            st.write(bot_template.replace(
+            chat_display.write(bot_template.replace(
                 "{{MSG}}", message.content), unsafe_allow_html=True)
 
 
